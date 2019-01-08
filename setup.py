6391ff8b7badb781f652462f1daf5daec6428b33
Brian Winkler <brianewinkler@gmail.com>
2019-01-08 12:12:01 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MakeParallel',
+      version='0.1',
+      description='Utilize multiple cpu cores',
+      url='https://github.com/BrighBrigh/MakeParallel',
+      author='Brian Winkler',
+      author_email='brianewinkler@gmail.com',
+      license='MIT',
+      packages=['MakeParallel'],
+      zip_safe=False)
