d3f505a7576ad3ba1084145d59357a801a03a3be
Brian Winkler <brianewinkler@gmail.com>
2019-01-08 12:57:30 -0700

    Update __init__.py

--- MakeParallel/__init__.py
+++ MakeParallel/__init__.py
@@ -1,7 +1,18 @@
-from multiprocessing import Process
+from multiprocessing import Process, cpu_count, Pool
 import os
 import pandas as pd
+import numpy as np
 
 def MakeParallel(function, argu):
     p = Process(target=function, args=argu)
     p.start()
+
+def parallelize(data, func):
+    cores = cpu_count() #Number of CPU cores on your system
+    partitions = cores #Define as many partitions as you want
+    data_split = np.array_split(data, partitions)
+    pool = Pool(cores)
+    data = pd.concat(pool.map(func, data_split))
+    pool.close()
+    pool.join()
+    return data
